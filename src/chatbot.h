73dd6b18362c7156f69d962139cf072bddd1c038
Jason Reich <jreich218@gmail.com>
2022-08-14 00:22:25 -0700

    finished tasks

--- src/chatbot.h
+++ src/chatbot.h
@@ -7,41 +7,54 @@
 class GraphNode; // forward declaration
 class ChatLogic; // forward declaration
 
-class ChatBot
-{
-private:
-    // data handles (owned)
-    wxBitmap *_image; // avatar image
+class ChatBot {
+ private:
+  // data handles (owned)
+  wxBitmap *_image; // avatar image
 
-    // data handles (not owned)
-    GraphNode *_currentNode;
-    GraphNode *_rootNode;
-    ChatLogic *_chatLogic;
+  // data handles (not owned)
+  GraphNode *_currentNode;
+  GraphNode *_rootNode;
+  ChatLogic *_chatLogic;
 
-    // proprietary functions
-    int ComputeLevenshteinDistance(std::string s1, std::string s2);
+  // proprietary functions
+  int ComputeLevenshteinDistance(std::string s1, std::string s2);
 
-public:
-    // constructors / destructors
-    ChatBot();                     // constructor WITHOUT memory allocation
-    ChatBot(std::string filename); // constructor WITH memory allocation
-    ~ChatBot();
+ public:
+  // constructors / destructors
+  ChatBot();                     // constructor WITHOUT memory allocation
+  explicit ChatBot(std::string filename); // constructor WITH memory allocation
 
-    //// STUDENT CODE
-    ////
+  /**
+   * Rule of 5
+   */
+  // 1. Copy constructor
+  ChatBot(const ChatBot &rhs);
+  // 2. Move constructor
+  ChatBot(ChatBot &&rhs) noexcept;
+  // 3. Copy assignment
+  ChatBot &operator=(const ChatBot &rhs);
+  // 4. Move assignment
+  ChatBot &operator=(ChatBot &&rhs) noexcept;
+  // 5. Destructor
+  ~ChatBot();
 
-    ////
-    //// EOF STUDENT CODE
 
-    // getters / setters
-    void SetCurrentNode(GraphNode *node);
-    void SetRootNode(GraphNode *rootNode) { _rootNode = rootNode; }
-    void SetChatLogicHandle(ChatLogic *chatLogic) { _chatLogic = chatLogic; }
-    ChatLogic* GetChatLogicHandle() { return _chatLogic; }
-    wxBitmap *GetImageHandle() { return _image; }
+  //// STUDENT CODE
+  ////
 
-    // communication
-    void ReceiveMessageFromUser(std::string message);
+  ////
+  //// EOF STUDENT CODE
+
+  // getters / setters
+  void SetCurrentNode(GraphNode *node);
+  void SetRootNode(GraphNode *rootNode) { _rootNode = rootNode; }
+  void SetChatLogicHandle(ChatLogic *chatLogic) { _chatLogic = chatLogic; }
+  ChatLogic *GetChatLogicHandle() { return _chatLogic; }
+  wxBitmap *GetImageHandle() { return _image; }
+
+  // communication
+  void ReceiveMessageFromUser(std::string message);
 };
 
 #endif /* CHATBOT_H_ */
--- src/graphnode.cpp
+++ src/graphnode.cpp
@@ -1,60 +1,54 @@
 #include "graphedge.h"
 #include "graphnode.h"
 
-GraphNode::GraphNode(int id)
-{
-    _id = id;
+GraphNode::GraphNode(int id) {
+  _id = id;
 }
 
-GraphNode::~GraphNode()
-{
-    //// STUDENT CODE
-    ////
+GraphNode::~GraphNode() {
+  //// STUDENT CODE
+  ////
 
-    delete _chatBot; 
+  // The below line was the bug that occurred when closing the app (aka the "warm up problem").
+  // It was a bug because this class doesn't put anything on the heap for _chatBot (i.e. this class isn't
+  // responsible for deallocating the heap memory created for _chatBot)
+  // delete _chatBot;
 
-    ////
-    //// EOF STUDENT CODE
+  ////
+  //// EOF STUDENT CODE
 }
 
-void GraphNode::AddToken(std::string token)
-{
-    _answers.push_back(token);
+void GraphNode::AddToken(std::string token) {
+  _answers.push_back(token);
 }
 
-void GraphNode::AddEdgeToParentNode(GraphEdge *edge)
-{
-    _parentEdges.push_back(edge);
+void GraphNode::AddEdgeToParentNode(GraphEdge *edge) {
+  _parentEdges.push_back(edge);
 }
 
-void GraphNode::AddEdgeToChildNode(GraphEdge *edge)
-{
-    _childEdges.push_back(edge);
+void GraphNode::AddEdgeToChildNode(std::unique_ptr<GraphEdge> edge) {
+  _childEdges.push_back(std::move(edge));
 }
 
 //// STUDENT CODE
 ////
-void GraphNode::MoveChatbotHere(ChatBot *chatbot)
-{
-    _chatBot = chatbot;
-    _chatBot->SetCurrentNode(this);
+void GraphNode::MoveChatbotHere(ChatBot chatbot) {
+  _chatBot = std::move(chatbot);
+  _chatBot.SetCurrentNode(this);
 }
 
-void GraphNode::MoveChatbotToNewNode(GraphNode *newNode)
-{
-    newNode->MoveChatbotHere(_chatBot);
-    _chatBot = nullptr; // invalidate pointer at source
+void GraphNode::MoveChatbotToNewNode(GraphNode *newNode) {
+  newNode->MoveChatbotHere(std::move(_chatBot));
 }
 ////
 //// EOF STUDENT CODE
 
-GraphEdge *GraphNode::GetChildEdgeAtIndex(int index)
-{
-    //// STUDENT CODE
-    ////
+GraphEdge *GraphNode::GetChildEdgeAtIndex(int index) {
+  //// STUDENT CODE
+  ////
 
-    return _childEdges[index];
+  return (_childEdges[index]).get();
 
-    ////
-    //// EOF STUDENT CODE
+  ////
+  //// EOF STUDENT CODE
 }
